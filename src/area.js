21e3ecefdb2846a67c6484ce54226a81bb4ad9dc
sankalp <sankalpdixit@cedcoss.com>
2022-02-04 18:49:12 +0530

    calculator

---
+++ src/area.js
@@ -0,0 +1,8 @@
+function calculate() {
+number_1 = Number(document.getElementById("firstno").value);
+number_2 = Number(document.getElementById("secondno").value);
+var area = number_1 * number_2;
+var perimeter  = 2*(number_1+number_2);
+document.getElementById("lblArea").innerHTML = area;
+document.getElementById("lblPerimeter").innerHTML = perimeter;
+}
